7e3b2f41b8ad5f055ffabd40fb93a8a80089d479
zayonvx <zayonvx@gmail.com>
2020-09-07 23:35:48 +0300

    stage #2

--- SimpleBot.java
+++ SimpleBot.java
@@ -6,12 +6,15 @@ public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2018.");
+        String botName = "Vlad";
+        String botBirthName = "2020";
+
+        System.out.println("Hello! My name is " + botName);
+        System.out.println("I was created in " + botBirthName + ".");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
+        String userName = scanner.nextLine();
 
-        System.out.println("What a great name you have, {yourName}!");
+        System.out.println("What a great name you have, " + userName + "!");
     }
 }
