0111dd77ccf49d8d6f3b009aa26bad4800edf93a
zayonvx <zayonvx@gmail.com>
2020-09-07 23:31:04 +0300

    stage #1

--- SimpleBot.java
+++ SimpleBot.java
@@ -2,6 +2,9 @@ package bot;
 
 public class SimpleBot {
     public static void main(String[] args) {
-        // write your code here
+        String botName = "Vlad";
+        String birthYear = "2020";
+        System.out.println("Hello! My name is " + botName + "\n" +
+                "I was created in " + birthYear);
     }
 }
